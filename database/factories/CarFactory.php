ef091b74f532d613733f2da05ecd8a642f83991a
Volodymyr Lench <volodymyr.lench@binary-studio.com>
2018-06-01 21:34:41 +0300

    Add factory of car

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -13,11 +13,10 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Entity\Car::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'brand' => $faker->randomElement(['BMW', 'Mercedes', 'ZAZ']),
+        'model' => $faker->randomElement(['x3', 'x5', 'x6']),
+        'price' => $faker->randomFloat()
     ];
 });
