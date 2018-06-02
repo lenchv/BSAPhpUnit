ffc7e9d5607b0d79b8fd7e8286188977c8310490
Volodymyr Lench <volodymyr.lench@binary-studio.com>
2018-06-02 21:24:55 +0300

    Fix DI

--- src/GradeService.php
+++ src/GradeService.php
@@ -3,16 +3,19 @@ declare(strict_types=1);
 
 namespace BSA;
 
+use BSA\Contracts\IGradeBook;
+use BSA\Contracts\IStudent;
+
 class GradeService
 {
     private $gradeBook;
 
-    public function __construct(GradeBook $gradeBook)
+    public function __construct(IGradeBook $gradeBook)
     {
         $this->gradeBook = $gradeBook;
     }
 
-    public function averageGrades(Student $student) : float
+    public function averageGrades(IStudent $student) : float
     {
         $grades = $this->gradeBook->gradesFor($student);
 
